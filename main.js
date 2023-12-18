454126ce4b6e8dda5812909d7aa91eb884f82aac
leamontoya19 <leamontoya19@gmail.com>
2023-12-18 23:36:30 +0100

    card

---
+++ main.js
@@ -0,0 +1,19 @@
+function createSnow(){
+    const snow = document.createElement('div');
+    snow.classList.add('snow'); 
+    snow.style.left = Math.random()*100+'vh';
+    snow.style.animationDuration = Math.random()+2+3+'s';
+    snow.innerText = '❄'
+    document.body.appendChild(snow)
+}
+setInterval(createSnow, 1000)
+
+//gsap
+
+const tl = gsap.timeline({default:{duration:.75}});
+tl.fromTo(".text",{scale:0},{scale:1,ease:"elastic.out(1,0,3",duration:1});
+tl.fromTo(".title", {y:100, opacity:0}, {y:0, opacity:1});
+tl.fromTo(".subtitle",{x:100, opacity:0},{x:0,opacity:1},"<");
+tl.fromTo(".logo-tree",{opacity:0,y:100},{opacity:1,y:0});
+tl.fromTo(".logo-tree",{y:0},{y:-10,repeat:-1, yoyo:true});
+tl.fromTo(".topping",{rotation:"-2deg"},{rotation:"2deg",repeat:-1,yoyo:true},"<")
