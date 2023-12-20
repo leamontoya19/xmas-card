e564ce3488433ccfd0d41440b4927397f0fb9406
leamontoya19 <leamontoya19@gmail.com>
2023-12-20 23:14:06 +0100

    audio

--- main.js
+++ main.js
@@ -17,3 +17,14 @@ tl.fromTo(".subtitle",{x:100, opacity:0},{x:0,opacity:1},"<");
 tl.fromTo(".logo-tree",{opacity:0,y:100},{opacity:1,y:0});
 tl.fromTo(".logo-tree",{y:0},{y:-10,repeat:-1, yoyo:true});
 tl.fromTo(".topping",{rotation:"-2deg"},{rotation:"2deg",repeat:-1,yoyo:true},"<")
+
+//audio
+
+window.addEventListener("DOMContentLoaded", function() {
+    // Obtener el elemento de audio
+    const audio = document.getElementById("audio");
+    
+    // Reproducir el audio cuando la página se cargue
+    audio.play();
+  });
+  
